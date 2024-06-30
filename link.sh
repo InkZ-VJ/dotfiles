54392756413ced356ffeb1cb63fb2fb77ebf9242
Ink-Vatcharapong Jittiprasert <vatcharapong.j@7solutions.co.th>
2024-06-30 20:42:39 +0700

    add readme

--- link.sh
+++ link.sh
@@ -5,5 +5,6 @@ DOTFILES_DIR=~/dotfiles
 ln -sf $DOTFILES_DIR/.zshrc ~/.zshrc
 ln -sf $DOTFILES_DIR/.tmux.conf ~/.tmux.conf
 ln -sf $DOTFILES_DIR/nvim ~/.config/nvim
+ln -sf $DOTFILES_DIR/fish ~/.config/fish
 ln -sf $DOTFILES_DIR/alacritty ~/.config/alacritty
 ln -sf $DOTFILES_DIR/ohmyposh ~/.config/ohmyposh
---
+++ readme.md
@@ -0,0 +1,11 @@
+# Inkz-Dotfiles
+
+## installation
+
+```bash
+git clone https://github.com/InkZ-VJ/dotfiles ~/dotfiles 
+cd ~/dotfiles
+chmod +x link.sh
+./link.sh
+```
+
