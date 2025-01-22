5680c652520b4499881d4a1bc281bedc608f76ce
InkZ-VJ <vatcharapong.j@hotmail.com>
2025-01-22 09:46:24 +0700

    feature: update nvim harpoon vim cmd

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,45 +1,45 @@
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
 -- template setup
-require('setting.template')
+require("setting.template")
 
-require('setting.keymap')
+require("setting.keymap")
 -- remap for colemak Users
-require('colemak')
-
+require("colemak")
 
 -- Lazy Package Maneger
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system {
-        'git',
-        'clone',
-        '--filter=blob:none',
-        'https://github.com/folke/lazy.nvim.git',
-        '--branch=stable', -- latest stable release
-        lazypath,
-    }
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 
 vim.opt.rtp:prepend(lazypath)
 
-require('lazy').setup({
-    require 'plugins.theme',
-    require 'plugins.autoformat',
-    require 'plugins.lsp',
-    require 'plugins.mini',
-    require 'plugins.autocomplete',
-    require 'plugins.telescope',
-    require 'plugins.treesitter',
-    require 'plugins.noice',
-    require 'plugins.snack',
-    require 'plugins.fileExplore',
-    require 'plugins.undo',
-    require 'plugins.fileNavigator',
-    require 'plugins.git',
-    require 'plugins.copilot',
-    require 'plugins.debug',
+require("lazy").setup({
+	require("plugins.theme"),
+	require("plugins.autoformat"),
+	require("plugins.lsp"),
+	require("plugins.mini"),
+	require("plugins.autocomplete"),
+	require("plugins.telescope"),
+	require("plugins.treesitter"),
+	require("plugins.noice"),
+	require("plugins.snack"),
+	require("plugins.fileExplore"),
+	require("plugins.undo"),
+	require("plugins.fileNavigator"),
+	require("plugins.git"),
+	require("plugins.copilot"),
+	require("plugins.debug"),
 })
 
-vim.cmd.colorscheme 'catppuccin'
+vim.cmd.colorscheme("catppuccin")
+-- vim.cmd.colorscheme("rose-pine")
--- .config/nvim/lua/setting/aerial/init.lua
+++ .config/nvim/lua/setting/aerial/init.lua
@@ -1,39 +1,39 @@
 require("aerial").setup({
-    layout = {
-        -- These control the width of the aerial window.
-        -- They can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
-        -- min_width and max_width can be a list of mixed types.
-        -- max_width = {40, 0.2} means "the lesser of 40 columns or 20% of total"
-        max_width = { 40, 0.2 },
-        width = nil,
-        min_width = 20,
+	layout = {
+		-- These control the width of the aerial window.
+		-- They can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
+		-- min_width and max_width can be a list of mixed types.
+		-- max_width = {40, 0.2} means "the lesser of 40 columns or 20% of total"
+		max_width = { 60, 0.2 },
+		width = nil,
+		min_width = 40,
 
-        -- key-value pairs of window-local options for aerial window (e.g. winhl)
-        win_opts = {},
+		-- key-value pairs of window-local options for aerial window (e.g. winhl)
+		win_opts = {},
 
-        -- Determines the default direction to open the aerial window. The 'prefer'
-        -- options will open the window in the other direction *if* there is a
-        -- different buffer in the way of the preferred direction
-        -- Enum: prefer_right, prefer_left, right, left, float
-        default_direction = "left",
+		-- Determines the default direction to open the aerial window. The 'prefer'
+		-- options will open the window in the other direction *if* there is a
+		-- different buffer in the way of the preferred direction
+		-- Enum: prefer_right, prefer_left, right, left, float
+		default_direction = "left",
 
-        -- Determines where the aerial window will be opened
-        --   edge   - open aerial at the far right/left of the editor
-        --   window - open aerial to the right/left of the current window
-        placement = "window",
+		-- Determines where the aerial window will be opened
+		--   edge   - open aerial at the far right/left of the editor
+		--   window - open aerial to the right/left of the current window
+		placement = "window",
 
-        -- When the symbols change, resize the aerial window (within min/max constraints) to fit
-        resize_to_content = true,
+		-- When the symbols change, resize the aerial window (within min/max constraints) to fit
+		resize_to_content = true,
 
-        -- Preserve window size equality with (:help CTRL-W_=)
-        preserve_equality = false,
-    },
-    -- optionally use on_attach to set keymaps when aerial has attached to a buffer
-    on_attach = function(bufnr)
-        -- Jump forwards/backwards with '{' and '}'
-        vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
-        vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
-    end,
+		-- Preserve window size equality with (:help CTRL-W_=)
+		preserve_equality = false,
+	},
+	-- optionally use on_attach to set keymaps when aerial has attached to a buffer
+	on_attach = function(bufnr)
+		-- Jump forwards/backwards with '{' and '}'
+		vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
+		vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
+	end,
 })
 
 -- You probably also want to set a keymap to toggle aerial
